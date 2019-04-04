52bbb82515134476b6ead575a7504ba6fc28eaf6
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2019-04-04 23:08:15 +0200

    Map crime with date

--- libs/Mapper.js
+++ libs/Mapper.js
@@ -1,16 +1,19 @@
 // @flow
+import {apiTimeFormat} from './moment'
+
 export let mapCrime = (obj: Object): Object => {
   return obj.map((item, index) => {
-    let {location} = item
+    let {location, datetime} = item
     let title = item.name
     let {name, gps} = location
     let latitude = gps.split(',')[0]
     let longitude = gps.split(',')[1]
+    datetime = apiTimeFormat(datetime)
     location = {
       name,
       latitude,
       longitude
-    }
-    return {...item, title, location}
+    }  
+    return {...item, datetime, title, location}
   })
 }
