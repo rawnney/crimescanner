6f1ae094d4b77b2916881630e5fd19869e7072a0
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2019-04-07 16:26:00 +0200

    gql init apollo, new client inside App

--- components/App.js
+++ components/App.js
@@ -1,19 +1,25 @@
 // @flow
 import {PureComponent} from 'react'
 import {ApolloProvider} from 'react-apollo'
-import {client} from '../libs/ApolloClient'
+import ApolloClient from 'apollo-boost'
+// import {client} from '../libs/ApolloClient'
 import {getStackNavigator} from '../libs/getStackNavigator'
 import {setAppNavRef} from '../libs/AppNavigation'
 import {createAppContainer} from 'react-navigation'
+import {GQL_SIMPLE_API_KEY} from '../consts/ApiKeys'
 
 type Props = {}
 type State = {}
 
 const AppContainer = createAppContainer(getStackNavigator())
 
+const localClient = new ApolloClient({
+  uri: GQL_SIMPLE_API_KEY
+})
+
 export default class App extends PureComponent<Props, State> {
   render (): React$Element<*> {
-    return <ApolloProvider client={client}>
+    return <ApolloProvider client={localClient}>
       <AppContainer ref={setAppNavRef} />
     </ApolloProvider>
   }
--- libs/ApolloClient.js
+++ libs/ApolloClient.js
@@ -1,14 +1,15 @@
 // @flow
 import {GQL_SIMPLE_API_KEY} from '../consts/ApiKeys'
-import {ApolloClient} from 'apollo-client'
-import {HttpLink} from 'apollo-link-http'
-import {InMemoryCache} from 'apollo-cache-inmemory'
+import {ApolloClient} from 'apollo-boost'
+// import {HttpLink} from 'apollo-link-http'
+// import {InMemoryCache} from 'apollo-cache-inmemory'
 
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
-const httpLink = new HttpLink({uri: GQL_SIMPLE_API_KEY}, fetch)
+// const httpLink = new HttpLink({uri: GQL_SIMPLE_API_KEY}, fetch)
 
 export let client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+  uri: GQL_SIMPLE_API_KEY
+  // link: httpLink,
+  // cache: new InMemoryCache()
 })
