f7ac69233eb24a1d7829ac9a3c51c03936873d43
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2019-04-09 22:55:41 +0200

    Update UserSettingsContainer.js

--- components/UserSettingsContainer.js
+++ components/UserSettingsContainer.js
@@ -8,11 +8,12 @@ import RowSwitch from './RowSwitch'
 import Store from '../libs/Store'
 import * as Actions from '../libs/Actions'
 import getColorTheme from '../libs/getColorTheme'
+import {connect} from 'react-redux'
 
 type Props = {}
 type State = {}
 
-export default class UserSettingsContainer extends PureComponent<Props, State> {
+class UserSettingsContainer extends PureComponent<Props, State> {
   static routeName = 'UserSettingsContainer'
   static navigationOptions = (state: *) => ({
     ...getDefaultNavigationOptions(state)
@@ -27,8 +28,10 @@ export default class UserSettingsContainer extends PureComponent<Props, State> {
     </View>
   }
 
-  toggle = (isDarkMode: boolean) => Store.dispatch(Actions.changeAppState({colorTheme: !isDarkMode}))
+  toggle = (isDarkMode: boolean) => Store.dispatch(Actions.changeAppState({isDarkMode: isDarkMode}))
 }
+
+export default connect(state => state)(UserSettingsContainer)
 
 const styles = StyleSheet.create({
   container: {
