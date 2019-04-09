9441c405ac5b9e780c73719c2f9b61c9ce02a8f9
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2019-04-09 22:52:24 +0200

    Update Fonts.js

--- libs/Fonts.js
+++ libs/Fonts.js
@@ -3,31 +3,26 @@ import {isIOS} from './Common'
 import colors from './Colors'
 
 let defaultFont = isIOS() ? 'Thonburi-Light' : 'Roboto'
-let defaultColor = colors.black
 
 export default {
   regular: {
     fontSize: 14,
     fontWeight: '400',
-    fontFamily: defaultFont,
-    color: defaultColor
+    fontFamily: defaultFont
   },
   semiBold: {
     fontSize: 14,
     fontWeight: '600',
-    fontFamily: defaultFont,
-    color: defaultColor
+    fontFamily: defaultFont
   },
   bold: {
     fontSize: 14,
     fontWeight: '700',
-    fontFamily: defaultFont,
-    color: defaultColor
+    fontFamily: defaultFont
   },
   light: {
     fontSize: 14,
     fontWeight: '100',
-    fontFamily: defaultFont,
-    color: defaultColor
+    fontFamily: defaultFont
   }
 }
