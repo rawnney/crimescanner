5426408704186910f6880a7c28c3e2230a89ab8e
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2019-05-12 22:27:54 +0200

    lint

--- flow/decl.js
+++ flow/decl.js
@@ -4,4 +4,4 @@ declare module 'react-native' { declare module.exports: * }
 declare var React: $Exports<'react'>
 declare var element: *
 declare module 'react-native-firebase' { declare module.exports: * }
-declare module 'react-native-permissions' {declare module.exports: * }
+declare module 'react-native-permissions' { declare module.exports: * }
